1c06f1ab9f42074f683249eb8cb205e86ba24dfd
Luis Guillermo Pérez Fuentes <luis.utl.15@gmail.com>
2025-08-16 13:29:19 -0600

    Lo logre resolver

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        // Instancia de tu clase
+        var solver = new WeightedUniformStringSolver();
+
+        // Cadena de prueba
+        string cadena = "abccba";
+
+        // Lista de queries (pesos a verificar)
+        List<int> queries = new List<int> { 1, 3, 9, 10 };
+
+        // Llamada al método
+        solver.WeightedUniformStrings(cadena, queries);
+    }
+}
--- src/WeightedUniformStrings.Core/WeightedUniformStringSolver.cs
+++ src/WeightedUniformStrings.Core/WeightedUniformStringSolver.cs
@@ -2,28 +2,70 @@
 
 public class WeightedUniformStringSolver
 {
-    public List<string> WeightedUniformStrings(string s, List<int> queries)
+
+    public static List<string> WeightedUniformStrings(string s, List<int> queries)
     {
-        List<string> letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-        List<int> valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
-        List<string> cadenas = new List<string>();
+        List<string> letras = new List<string>
+        {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s",
+            "t", "u", "v", "w", "x", "y", "z"
+        };
+
+        List<int> valores = new List<int>
+        {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26
+        };
+
+        List<Cadena> cadenas = new List<Cadena>();
+        List<int> u = new List<int>();
+        List<string> resp = new List<string>();
+        string temp = "";
+        string cadena = "";
+        int valor = 0;
+
         foreach (var letra in s)
         {
-            string cadena = "";
-            string temp = "";
+            int posicion = Array.IndexOf(letras.ToArray(), letra.ToString());
             if (letra.ToString() != temp)
             {
                 cadena = letra.ToString();
-                cadenas.Add(cadena);
-            }
-            else { 
-                cadena = cadena + letra.ToString();
-                cadenas.Add(cadena);
-            }
+                valor = valores[posicion];
 
+            }
+            else
+            {
+                cadena = cadena + "" + letra.ToString();
+                valor += valores[posicion];
+            }
+            cadenas.Add(new Cadena { cadena = cadena, valor = valor });
+            u.Add(valor);
             temp = letra.ToString();
+        }
+        int suma = cadenas.Sum(c => c.valor);
+
+        foreach (var q in queries)
+        {
+            bool existe = Array.Exists(u.ToArray(), n => n == q);
+            if (existe)
+            {
+                resp.Add("Yes");
+            }
+            else
+            {
+                resp.Add("No");
+            }
         }
 
 
+        return resp;
+    }
+
+    public class Cadena
+    {
+        public string cadena { get; set; }
+        public int valor { get; set; }
     }
 }
